8eaeba0e945c3b5c1f7528f865f735cde8861759
Nick Masluk <nick@randombytes.net>
2023-11-03 13:17:27 -0400

    Script corrections

--- quickstart.sh
+++ quickstart.sh
@@ -1,16 +1,35 @@
+#!/bin/bash
+
 HN="debian"
 
-kind delete cluster --name ks-core
-kind delete cluster --name ks-edge-cluster1
-kind delete cluster --name ks-edge-cluster2
+if [ -z $1 ]; then
+  echo "$0: missing directory to execute example in"
+  echo "Usage: $0 DIRECTORY"
+  exit 1
+fi
 
-KUBECONFIG=~/.kube/config kubectl config delete-context ks-core || true
-KUBECONFIG=~/.kube/config kubectl config delete-context ks-edge-cluster1 || true
-KUBECONFIG=~/.kube/config kubectl config delete-context ks-edge-cluster2 || true
+WORKING_DIR=$1
+
+cd $WORKING_DIR
+# ensure WORKING_DIR is an absolute path
+WORKING_DIR=$(pwd)
+
+echo "------------------------------------------------------------------------"
+echo "Running example in directory $WORKING_DIR"
+echo ""
+echo "KCP version: $(kcp --version)"
+echo "KCP location: $(which kcp)"
+echo ""
+echo "KubeStellar version: $(kubestellar-version)"
+echo "KubeStellar location: $(which kubestellar)"
+echo ""
+echo "------------------------------------------------------------------------"
 
 # Pre-req
 
-KUBECONFIG=~/.kube/config kind create cluster --name ks-core --config - <<EOF
+echo "*** Make kind for ks-core"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kind create cluster --name ks-core --config - <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
@@ -27,17 +46,25 @@ nodes:
     protocol: TCP
 EOF
 
-KUBECONFIG=~/.kube/config kubectl \
+echo "*** Create nginx ingress"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl \
   create -f https://raw.githubusercontent.com/kubestellar/kubestellar/main/example/kind-nginx-ingress-with-SSL-passthrough.yaml
 
-sleep 10
+echo "*** Wait 20 seconds"
 
-KUBECONFIG=~/.kube/config kubectl wait --namespace ingress-nginx \
+sleep 20
+
+echo "*** Wait for ingress"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
-KUBECONFIG=~/.kube/config kind create cluster --name ks-edge-cluster1 --config - <<EOF
+echo "*** Make kind ks-edge-cluster1"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kind create cluster --name ks-edge-cluster1 --config - <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
@@ -47,7 +74,9 @@ nodes:
     hostPort: 8094
 EOF
 
-KUBECONFIG=~/.kube/config kind create cluster --name ks-edge-cluster2 --config - <<EOF
+echo "*** Make kind ks-edge-cluster2"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kind create cluster --name ks-edge-cluster2 --config - <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
@@ -59,25 +88,31 @@ nodes:
     hostPort: 8097
 EOF
 
-KUBECONFIG=~/.kube/config kubectl config rename-context kind-ks-core ks-core
-KUBECONFIG=~/.kube/config kubectl config rename-context kind-ks-edge-cluster1 ks-edge-cluster1
-KUBECONFIG=~/.kube/config kubectl config rename-context kind-ks-edge-cluster2 ks-edge-cluster2
+echo "*** Rename kind clusters, removing 'kind-ks' prefix"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl config rename-context kind-ks-core ks-core
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl config rename-context kind-ks-edge-cluster1 ks-edge-cluster1
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl config rename-context kind-ks-edge-cluster2 ks-edge-cluster2
 
 # 1. Deploy the KUBESTELLAR Core component
 
-KUBECONFIG=~/.kube/config kubectl config use-context ks-core  
-KUBECONFIG=~/.kube/config kubectl create namespace kubestellar  
+echo "*** Use context 'ks-core', create namespace 'kubestellar'"
+
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl config use-context ks-core  
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl create namespace kubestellar  
+
+echo "*** Install Kubestellar core with Helm"
 
 helm repo add kubestellar https://helm.kubestellar.io
 helm repo update
-KUBECONFIG=~/.kube/config helm install kubestellar/kubestellar-core \
+KUBECONFIG=$WORKING_DIR/.kube/config helm install kubestellar/kubestellar-core \
   --set EXTERNAL_HOSTNAME=$HN \
   --set EXTERNAL_PORT=1119 \
   --namespace kubestellar \
   --generate-name
 
 echo -n 'Waiting for KubeStellar to be ready'
-while ! KUBECONFIG=~/.kube/config kubectl exec $(KUBECONFIG=~/.kube/config kubectl get pod \
+while ! KUBECONFIG=$WORKING_DIR/.kube/config kubectl exec $(KUBECONFIG=$WORKING_DIR/.kube/config kubectl get pod \
    --selector=app=kubestellar -o jsonpath='{.items[0].metadata.name}' -n kubestellar) \
    -n kubestellar -c init -- ls /home/kubestellar/ready &> /dev/null; do
    sleep 10
@@ -86,48 +121,54 @@ done
 
 echo; echo; echo "KubeStellar is now ready to take requests"
 
-KUBECONFIG=~/.kube/config kubectl --context ks-core get secrets kubestellar \
-  -o jsonpath='{.data.external\.kubeconfig}' \
-  -n kubestellar | base64 -d > ks-core.kubeconfig
-
-KUBECONFIG=ks-core.kubeconfig kubectl ws --context root tree
-
 # 2. Install KUBESTELLAR's user commands and kubectl plugins
 # skip all the brew commands
 
 # 3. View your KUBESTELLAR Core Space environment
 
-KUBECONFIG=~/.kube/config kubectl --context ks-core get secrets kubestellar \
-  -o jsonpath='{.data.external\.kubeconfig}' \
-  -n kubestellar | base64 -d > ks-core.kubeconfig
+echo "*** Get secrets, dump to 'ks-core.kubeconfig'"
 
-KUBECONFIG=ks-core.kubeconfig kubectl ws --context root tree
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-core get secrets kubestellar \
+  -o jsonpath='{.data.external\.kubeconfig}' \
+  -n kubestellar | base64 -d > $WORKING_DIR/ks-core.kubeconfig
+
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl ws --context root tree
 
 # 4. Install KUBESTELLAR Syncers on your Edge Clusters
 
-KUBECONFIG=ks-core.kubeconfig kubectl kubestellar prep-for-cluster --imw root:imw1 ks-edge-cluster1 \
+echo "*** Prepare syncers for ks-edge-cluster1 and ks-edge-cluster2"
+
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl kubestellar prep-for-cluster --imw root:imw1 ks-edge-cluster1 \
   env=ks-edge-cluster1 \
   location-group=edge     #add ks-edge-cluster1 and ks-edge-cluster2 to the same group
 
-KUBECONFIG=ks-core.kubeconfig kubectl kubestellar prep-for-cluster --imw root:imw1 ks-edge-cluster2 \
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl kubestellar prep-for-cluster --imw root:imw1 ks-edge-cluster2 \
   env=ks-edge-cluster2 \
   location-group=edge     #add ks-edge-cluster1 and ks-edge-cluster2 to the same group
 
+echo "*** Apply syncer to ks-edge-cluster1"
+
 #apply ks-edge-cluster1 syncer
-KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster1 apply -f ks-edge-cluster1-syncer.yaml
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster1 apply -f ks-edge-cluster1-syncer.yaml
 sleep 3
-KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster1 get pods -A | grep kubestellar  #check if syncer deployed to ks-edge-cluster1 correctly
+echo "*** Check if syncer deployed to ks-edge-cluster1"
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster1 get pods -A | grep kubestellar  #check if syncer deployed to ks-edge-cluster1 correctly
+
+echo "*** Apply syncer to ks-edge-cluster2"
 
 #apply ks-edge-cluster2 syncer
-KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster2 apply -f ks-edge-cluster2-syncer.yaml
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster2 apply -f ks-edge-cluster2-syncer.yaml
 sleep 3
-KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster2 get pods -A | grep kubestellar  #check if syncer deployed to ks-edge-cluster2 correctly
+echo "*** Check if syncer deployed to ks-edge-cluster2"
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster2 get pods -A | grep kubestellar  #check if syncer deployed to ks-edge-cluster2 correctly
 
 # 5. Deploy an Apache Web Server to ks-edge-cluster1 and ks-edge-cluster2
 
-KUBECONFIG=ks-core.kubeconfig kubectl ws root:wmw1
+echo "*** Create EdgePLacement"
 
-KUBECONFIG=ks-core.kubeconfig kubectl apply -f - <<EOF
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl ws root:wmw1
+
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl apply -f - <<EOF
 apiVersion: edge.kubestellar.io/v2alpha1
 kind: EdgePlacement
 metadata:
@@ -150,10 +191,14 @@ spec:
     objectNames: [ "bind-kubernetes", "bind-apps" ]
 EOF
 
-KUBECONFIG=ks-core.kubeconfig kubectl ws root:wmw1
-KUBECONFIG=ks-core.kubeconfig kubectl get edgeplacements -n kubestellar -o yaml
+echo "*** Check if EdgePlacement applied to ks-core"
 
-KUBECONFIG=ks-core.kubeconfig kubectl apply -f - <<EOF
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl ws root:wmw1
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl get edgeplacements -n kubestellar -o yaml
+
+echo "*** Apply workload to WDS on ks-core (wmw1)"
+
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -203,23 +248,29 @@ spec:
           optional: false
 EOF
 
-KUBECONFIG=ks-core.kubeconfig kubectl ws root:wmw1
-KUBECONFIG=ks-core.kubeconfig kubectl get deployments/my-first-kubestellar-deployment -n my-namespace -o yaml
-KUBECONFIG=ks-core.kubeconfig kubectl get deployments,cm -n my-namespace
+echo "*** Check if ConfigMap and Deployment applied to ks-core in 'my-namespace'"
+
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl ws root:wmw1
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl get deployments/my-first-kubestellar-deployment -n my-namespace -o yaml
+KUBECONFIG=$WORKING_DIR/ks-core.kubeconfig kubectl get deployments,cm -n my-namespace
 
 # 6. View the Apache Web Server running on ks-edge-cluster1 and ks-edge-cluster2
 
-KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster1 get deployments -A
+echo "*** Check that deployment was created on ks-edge-cluster1"
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster1 get deployments -A
 
-KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster2 get deployments -A
+echo "*** Check that deployment was created on ks-edge-cluster2"
+KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster2 get deployments -A
 
-while [[ $(KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster1 get pod \
+echo "*** Check that workload is running on  ks-edge-cluster1"
+while [[ $(KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster1 get pod \
   -l "app=common" -n my-namespace -o jsonpath='{.items[0].status.phase}') != "Running" ]]; do 
     sleep 5; 
   done;
 curl http://localhost:8094
 
-while [[ $(KUBECONFIG=~/.kube/config kubectl --context ks-edge-cluster2 get pod \
+echo "*** Check that workload is running on  ks-edge-cluster2"
+while [[ $(KUBECONFIG=$WORKING_DIR/.kube/config kubectl --context ks-edge-cluster2 get pod \
   -l "app=common" -n my-namespace -o jsonpath='{.items[0].status.phase}') != "Running" ]]; do 
     sleep 5; 
   done;
